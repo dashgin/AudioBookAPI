b506fdedb554333ca31f92781e178a2a42a353d7
Dashgin <dsdsds2103@gmail.com>
2021-12-25 20:08:59 +0400

    corrected syntax error at urls.py

--- audio_book/books/urls.py
+++ audio_book/books/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('levels/<int:id>/', LevelDetailView.as_view(), name='level_detail'),
     path('books/<int:id>/', BookDetailView.as_view(), name='book_detail'),
     path('chapters/<int:id>/', ChapterDetailView.as_view(), name='chapter_detail'),
-    path('versions/last/', last_version_view, name='last_version')'),
+    path('versions/last/', last_version_view, name='last_version'),
 ]
